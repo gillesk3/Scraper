52f9e9e5ff35b1b3937ff76f98cf65d071ccdbe6
Karl <gillespie.karl@hotmail.com>
2024-03-08 19:22:20 +0000

    Fixed worm including links in chapters

--- items.py
+++ items.py
@@ -2,16 +2,19 @@ import scrapy
 from scrapy.loader.processors import Join, MapCompose, TakeFirst
 import logging
 
+
 def serializeTitle(value):
-    a = a
-    print('*** IN SERIAILZER')
     return value.replace('\\xa0', ' ')
 
+
+def validContent(value):
+    if 'Last Chapter' not in value and 'a href' not in value:
+        return value
 
 
 class Chapter(scrapy.Item):
     Number = scrapy.Field()
-    Title = scrapy.Field(serializer=serializeTitle,output_processor=TakeFirst(),)
-    Header = scrapy.Field(output_processor=TakeFirst(),)
-    Content = scrapy.Field(output_processor=Join(),)
-    NextPage = scrapy.Field(output_processor=TakeFirst(),)
+    Title = scrapy.Field(serializer=serializeTitle, output_processor=TakeFirst(), )
+    Header = scrapy.Field(output_processor=TakeFirst(), )
+    Content = scrapy.Field(input_processor=MapCompose(validContent), output_processor=Join(), )
+    NextPage = scrapy.Field(output_processor=TakeFirst(), )
