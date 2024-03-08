a3116db0391939a6059ef827bfc8c58b736eadf7
Karl <gillespie.karl@hotmail.com>
2024-03-08 19:03:01 +0000

    Changed is not to !=

--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ from util import Settings
 import logging
 
 
-
 class EvilSpider(scrapy.Spider):
     name = 'serial'
     settings = Settings()
@@ -13,15 +12,13 @@ class EvilSpider(scrapy.Spider):
     chapterNumber = 0
     tableOfContents = "<h1>Table of Contents</h1>"
 
-
-
     def __init__(self, category='', **kwargs):
         if 'start_urls' in kwargs:
             self.start_urls = kwargs['start_urls']
         if 'filename' in kwargs:
             self.filename = kwargs['filename']
         if 'enendUrl' in kwargs:
-            self.endUrl = kwargs['enendUrl']
+            self.endUrl = kwargs['endUrl']
 
         super().__init__(**kwargs)  # python3
 
@@ -29,31 +26,29 @@ class EvilSpider(scrapy.Spider):
         chapter = ItemLoader(item=Chapter(), response=response)
         chapter.add_xpath('Title', '//h1[@class="entry-title"]/text()')
         # chapter.add_xpath('Content',  '//div[@class="entry-content"]/p[count(a)=0 and not( contains(.,//a))]/text()')
-        chapter.add_xpath('Content',  '//div[@class="entry-content"]/p')
-        chapter.add_xpath('NextPage','//a[@rel="next"]/@href')
+        chapter.add_xpath('Content', '//div[@class="entry-content"]/p')
+        chapter.add_xpath('NextPage', '//a[@rel="next"]/@href')
 
-        chapter.add_value('Number',self.chapterNumber)
+        chapter.add_value('Number', self.chapterNumber)
         chapter = chapter.load_item()
-
 
         if 'prologue' in chapter['Title'].lower() and self.chapterNumber != 0:
             return
 
-        chapter['Header'] =   self.setHeader(chapter['Title'])
+        chapter['Header'] = self.setHeader(chapter['Title'])
         self.tableOfContents += '<a href="#chap#%d">%s</a><br>' % (self.chapterNumber, chapter['Title'])
 
         yield chapter
         self.chapterNumber += 1
 
         if 'NextPage' in chapter:
-            if chapter['NextPage'] is not self.endUrl:
+            if chapter['NextPage'] != self.endUrl:
                 yield scrapy.Request(chapter['NextPage'], self.parse)
         else:
             print('no next page')
 
-
-    def setHeader(self,title):
+    def setHeader(self, title):
         if 'chapter' in title.lower():
             title = title.split(':')[1]
 
-        return '<h1 id="chap#{}">{}</h1>'.format(self.chapterNumber,  title)
+        return '<h1 id="chap#{}">{}</h1>'.format(self.chapterNumber, title)
